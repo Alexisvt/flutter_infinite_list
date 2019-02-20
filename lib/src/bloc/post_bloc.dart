a1373bce393019f6cde13c0083932b4d9612d293
Alexis Villegas Torres <alexisvt@gmail.com>
2019-02-20 14:11:14 -0600

    completing the bloc layer

--- lib/src/bloc/post_bloc.dart
+++ lib/src/bloc/post_bloc.dart
@@ -1,9 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:rxdart/rxdart.dart';
 
 import './post_events.dart';
 import './post_states.dart';
+import '../data/data.dart';
 
 class PostBloc extends Bloc<PostEvent, PostState> {
   final http.Client httpClient;
@@ -14,10 +17,52 @@ class PostBloc extends Bloc<PostEvent, PostState> {
   PostState get initialState => PostUninitialized();
 
   @override
+  Stream<PostEvent> transform(Stream<PostEvent> events) {
+    return (events as Observable<PostEvent>)
+        .debounce(Duration(milliseconds: 500));
+  }
+
+  @override
   Stream<PostState> mapEventToState(
     PostState currentState,
     PostEvent event,
   ) async* {
-    yield null;
+    try {
+      if (event is Fetch && !_hasReachedMax(currentState)) {
+        final List<Post> posts = await _fetchPosts(0, 20);
+        yield PostLoaded(posts: posts, hasReachedMax: false);
+      }
+
+      if (currentState is PostLoaded) {
+        final posts = await _fetchPosts(currentState.posts.length, 20);
+        yield posts.isEmpty
+            ? currentState.copyWith(hasReachedMax: true)
+            : PostLoaded(
+                posts: currentState.posts + posts, hasReachedMax: false);
+      }
+    } catch (_) {
+      yield PostError();
+    }
+  }
+
+  bool _hasReachedMax(PostState state) =>
+      state is PostLoaded && state.hasReachedMax;
+
+  Future<List<Post>> _fetchPosts(int startIndex, int limit) async {
+    final http.Response response = await httpClient.get(
+        'https://jsonplaceholder.typicode.com/posts?_start=$startIndex&_limit=$limit');
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body) as List;
+      return data.map((rawPost) {
+        return Post(
+          id: rawPost['id'],
+          tittle: rawPost['title'],
+          body: rawPost['body'],
+        );
+      }).toList();
+    } else {
+      throw Exception('error fetching posts');
+    }
   }
 }
